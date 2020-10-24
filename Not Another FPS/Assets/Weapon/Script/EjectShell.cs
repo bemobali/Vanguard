f22216e0ce976ca1aa3e6ca29a9fef8d33becfc6
bemobali <57385559+bemobali@users.noreply.github.com>
2020-10-23 20:17:51 -0700

    Shell ejection vector
    Fix shell ejection vector for a convincing rearward flip.

--- Not Another FPS/Assets/Weapon/Script/EjectShell.cs
+++ Not Another FPS/Assets/Weapon/Script/EjectShell.cs
@@ -19,7 +19,11 @@ public class EjectShell : MonoBehaviour
 		m_shellImpulsePoint = clone.transform.GetChild(0).gameObject;
 		if (rb)
 		{
-			rb.AddForceAtPosition(m_impulse, m_shellImpulsePoint.transform.position, ForceMode.Impulse);
+			Vector3 impulse = (m_ejectionPort.transform.right.normalized * m_impulse.x)			//x
+							+ (m_ejectionPort.transform.up.normalized * m_impulse.y)				//y
+							+ (m_ejectionPort.transform.forward.normalized * m_impulse.z);	//z
+
+			rb.AddForceAtPosition(impulse, m_shellImpulsePoint.transform.position, ForceMode.Impulse);
 			//rb.AddForceAtPosition(m_impulse, clone.transform.position, ForceMode.Impulse);
 		}
 	}
