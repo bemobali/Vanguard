936e67d2751632987d0199228ff3ca3351ad8804
bemobali <57385559+bemobali@users.noreply.github.com>
2020-10-28 22:55:59 -0700

    Improve HUD
    Change weapon static image size to match the aspec ratio of the weapon display area. Weapon display now look presentable. No more stubby SPAS-12 or slender AA-12.
    Shrink the health indicator for a nicer fit on the top-left corner of the HUD
    Change canvas resizing mode to match the display mode.

--- Not Another FPS/Assets/HUD/Script/HUD.cs
+++ Not Another FPS/Assets/HUD/Script/HUD.cs
@@ -12,9 +12,9 @@ public class HUD : MonoBehaviour
     RawImage m_currentWeaponImage;
     [SerializeField]
     Image m_healthSlider;
-    Color m_healthy = new Color(0f, 1f, 0.2013595f, 0.4980392f);
-    Color m_healthCaution = new Color(1f, 0.9398658f, 0.240566f, 0.4980392f);
-    Color m_healthCritical = new Color(0.7735849f, 0.174f, 0.08392668f, 0.4980392f);
+    Color m_healthy = new Color(0f, 1f, 0.2013595f, 0.5f);
+    Color m_healthCaution = new Color(1f, 0.9398658f, 0.240566f, 0.70f);
+    Color m_healthCritical = new Color(0.7735849f, 0.174f, 0.08392668f, 0.9f);
     // Start is called before the first frame update
     void Start()
     {
